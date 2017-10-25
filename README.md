55e0621ce106eee277e2499ab904f7f1debb81ff
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-10-25 20:43:32 +0200

    updated files
    comments and fine tuning.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # techdegree-project08
 Using Gulp to build a front end website
 
-This is the 8th of in total 12 projects I will complete through Treehouse's Fullstack Javascript techdegree. 
+This is the 8th of in total 12 projects I will complete through Treehouse's Fullstack Javascript techdegree.
 
 In this project:
 - Set up a Gulp build process
-- Concatenated and minified javascript and css files with source maps
+- Concatenated and minify Javascript and CSS files with source maps
 - Compile SCSS into CSS
 - Compress JPEG or PNG files
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Required modules
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
@@ -10,7 +11,11 @@ const del = require('del');
 const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const htmlReplace = require('gulp-html-replace');
+const replace = require('gulp-replace');
 
+// - Concatenate and minify Javascript files
+// - Generate source map
+// - Copy the files to the 'dist/scipts' folder
 gulp.task("scripts", () => {
   return gulp.src(['js/circle/*.js', 'js/global.js'])
   .pipe(maps.init())
@@ -20,6 +25,11 @@ gulp.task("scripts", () => {
   .pipe(gulp.dest('dist/scripts'));
 });
 
+// - Compile Sass into CSS
+// - Compress compiled code
+// - Generate source map
+// - Copy the files to the 'dist/styles' folder
+// - Prepare code to be reloaded (refresh the page)
 gulp.task("styles", () => {
   return gulp.src('sass/global.scss')
   .pipe(maps.init())
@@ -30,16 +40,20 @@ gulp.task("styles", () => {
   .pipe(browserSync.stream());
 });
 
+// - Compress image files
+// - Copy them to the 'dist/content' folder
 gulp.task("images", () => {
   return gulp.src('images/*')
   .pipe(imagemin())
-  .pipe(gulp.dest('dist/images'));
+  .pipe(gulp.dest('dist/content'));
 });
 
+// Delete everything in the 'dist' folder
 gulp.task("clean", () => {
   del('dist/*');
 });
 
+// Reload the page on Sass changes
 gulp.task("browser-sync", ["build"], () => {
     browserSync.init({
         server: "dist"
@@ -48,13 +62,17 @@ gulp.task("browser-sync", ["build"], () => {
     gulp.watch('*.html').on('change', browserSync.reload);
 });
 
+// - Runs the clean, scripts, images and styles tasks
+// - Adds all the icons to the 'dist' folder
 gulp.task("build", ["clean", "scripts", "images", "styles"], () => {
   gulp.src('icons/**', {base: './'})
   .pipe(gulp.dest('dist'));
 });
 
+// Updates the paths to the images, js and css in the 'dist' folder
 gulp.task("default", ["browser-sync"], () => {
   gulp.src('index.html')
+  .pipe(replace('images/', 'content/'))
   .pipe(htmlReplace({js: 'scripts/all.min.js', css: 'styles/all.min.css'}))
   .pipe(gulp.dest('dist'));
 });
